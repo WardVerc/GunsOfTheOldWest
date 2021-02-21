520c10c4746e1cccb9b32edbf589670e5d9cde62
Ward Vercruyssen <r0435477@student.vives.be>
2021-02-21 18:39:24 +0100

    Homepagina met counter + decrement functie + verwijzing naar verkooppagina bij counter = 0.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Guns_of_the_Old_West.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static int counter = 13;
+        
+        public IActionResult Index()
+        {
+            if (counter == 0)
+            {
+                return RedirectToAction("Verkoop");
+            }
+            
+            Decrement();
+            return View(counter);
+        }
+
+        public void Decrement()
+        {
+            counter--;
+        }
+
+        [HttpGet]
+        public IActionResult Verkoop()
+        {
+            return View();
+        }
+
+        
+    }
+}
